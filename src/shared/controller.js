e10071f1e55141f781fa8d36e33acbcccd29d1ed
LucaGiona <luca@senzproduction.com>
2024-08-30 16:52:49 +0200

    impl validation + clearbtn --on view.js + controller.js

--- src/shared/controller.js
+++ src/shared/controller.js
@@ -16,7 +16,23 @@ export default class Controller{
   return controller
  }
 
+ #isValid(data){
+   return data.name && data.age && data.email
+
+ }
+
+ #onSubmit({ name, age, email}){
+  if(!this.#isValid({name, age, email})){
+   this.#view.notify({msg: "Please fill out all the fields!!"})
+   return
+  }
+   this.#view.addRow({ name, age, email })
+ }
+
  #init(){
+  this.#view.configureFormSubmit(this.#onSubmit.bind(this))
+  this.#view.configureFormClear()
+
   const initialData = [
    { name: "Luca Giona", age: 50, email: "test@data.com"},
    { name: "Luca Maria", age: 23, email: "test@data.eu"},
